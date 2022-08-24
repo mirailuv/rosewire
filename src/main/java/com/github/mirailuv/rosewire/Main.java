d8ad6077fb13ce78f97ce21df9c2ea7ed6cbdeb6
mirailuv <mirailuvv@outlook.com>
2022-08-24 22:39:08 +0300

    things

---
+++ src/main/java/com/github/mirailuv/rosewire/Main.java
@@ -0,0 +1,16 @@
+package com.github.mirailuv.rosewire;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class Main implements DedicatedServerModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("rosewire");
+
+    @Override
+    public void onInitializeServer() {
+
+    }
+}
