764fb602b4e36dbc9178b3c5b7ad1f09a5a5a71b
mirailuv <mirailuvv@outlook.com>
2022-08-28 17:32:58 +0300

    added configurable prelaunch download

--- src/main/java/com/github/mirailuv/rosewire/FileManager.java
+++ src/main/java/com/github/mirailuv/rosewire/FileManager.java
@@ -137,6 +137,54 @@ public class FileManager {
             } catch (IOException e) {}
         }
     }
+
+    public static void preLaunch() {
+        File x = new File("./config/rosewire.prelaunch");
+        if (x.exists()) {
+            try {
+                r = new BufferedReader(new FileReader(x));
+            } catch (FileNotFoundException e) {}
+            String line = null;
+            boolean b = true;
+            while (b) {
+                line = null;
+                try {
+                    line = r.readLine();
+                } catch (IOException e) {}
+                if (line == null) b=false; else preDl(line);
+            }
+            try {
+                r.close();
+            } catch (IOException e) {}
+        } else {
+            File configFolder = new File("./config/");
+            configFolder.mkdirs();
+            try {
+                x.createNewFile();
+            } catch (IOException e) {}
+        }
+    }
+
+    public static void preDl(String file) {
+        if (exists(file)) {
+            String path = line1(file);
+            String link = line2(file);
+            String action = line3(file);
+            if (link != null) {
+                try {
+                    download(path,link);
+                } catch (IOException e) {}
+                if (action != null) {
+                    if (action.equals("unzip")) {
+                        String target = line4(file);
+                        try {
+                            unzip(path,target);
+                        } catch (IOException e) {}
+                    }                    
+                }
+            }
+        }
+    }
     
     public static void download(String file, String link) throws IOException {
             URL url = new URL(link);
---
+++ src/main/java/com/github/mirailuv/rosewire/PreLaunch.java
@@ -0,0 +1,9 @@
+package com.github.mirailuv.rosewire;
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+
+public class PreLaunch implements PreLaunchEntrypoint {
+    @Override
+    public void onPreLaunch() {
+        FileManager.preLaunch();
+    }
+}
